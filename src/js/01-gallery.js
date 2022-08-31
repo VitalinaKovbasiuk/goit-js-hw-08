c3fd0ae5e7b099adebc2fb7eeadbdf3b547f7fb8
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-08-31 14:02:03 +0300

    Підключаю бібліотеку - Завдання 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,48 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+
+const galleryUl = document.querySelector('.gallery');
+
+function createGalleryItems(items) {
+    return items
+    .map(
+        (item) => `<a class="gallery__item" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+      </a>`
+  )
+    .join('');
+}
+
+
+
+const addGalleryItems = createGalleryItems(galleryItems);
+// galleryDiv.innerHTML = addGalleryItems;
+
+function blockStandartAction(event) {
+    event.preventDefault();
+  }
+
+galleryUl.addEventListener('click', onImageClick);
+galleryUl.insertAdjacentHTML("beforeend", addGalleryItems);
+
+function onImageClick(event) {
+  blockStandartAction(event);
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    animationSpeed: 250,
+ });
+
+
+  galleryUl.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+
