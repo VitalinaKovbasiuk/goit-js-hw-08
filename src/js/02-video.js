87ecd47aa3149d5716ffff182c2860146f82fa78
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-09-01 16:25:32 +0300

    Додаю умови задач

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,34 @@
+
+// HTML містить <iframe> з відео для Vimeo плеєра. 
+// Напиши скрипт, який буде зберігати поточний час відтворення відео у локальне сховище і, 
+// після перезавантаження сторінки, продовжувати відтворювати відео з цього часу.
+// Виконуй це завдання у файлах 02-video.html і 02-video.js. Розбий його на декілька підзавдань:
+
+// 1.Ознайомся з документацією бібліотеки Vimeo плеєра.
+// 2.Додай бібліотеку як залежність проекту через npm - npm i --save lodash.throttle (Using npm:
+// $ npm i -g npm
+// $ npm i lodash
+// Note: add --save if you are using npm < 5.0.0)
+// 3.Ініціалізуй плеєр у файлі скрипта як це описано в секції pre-existing player, 
+// але враховуй, що у тебе плеєр доданий як npm пакет, а не через CDN.
+// 4.Вивчи документацію методу on() і почни відстежувати подію timeupdate - оновлення часу відтворення.
+// 5.Зберігай час відтворення у локальне сховище. Нехай ключем для сховища буде рядок "videoplayer-current-time".
+// 6.Під час перезавантаження сторінки скористайся методом setCurrentTime() 
+// з метою відновлення відтворення зі збереженої позиції.
+// 7.Додай до проекту бібілотеку lodash.throttle і зроби так, 
+// щоб час відтворення оновлювався у сховищі не частіше, ніж раз на секунду.
+
+
+
 // import throtle from 'lodash.throttle';
 // import Player from '@vimeo/player';
 
-// const playerIframeRef = document.querySelector('#vimeo-player');
-// const player = new Player(playerIframeRef, { autoplay: true });
+// const playerIframe = document.querySelector('#vimeo-player');
+// const player = new Player(playerIframe, { autoplay: true });
 
-// const PLAYER_STATE_LS_KEY = 'player-state';
+// const PLAYER_STATE = 'player-state';
 
-// let playerState = JSON.parse(localStorage.getItem(PLAYER_STATE_LS_KEY)) ?? {};
+// let playerState = JSON.parse(localStorage.getItem(PLAYER_STATE)) ?? {};
 
 // const setPlayerStateFromLocalStorage = () => {
 //   if (Object.keys(playerState).length === 0) return;
@@ -14,7 +36,6 @@
 //   player.setCurrentTime(playerState.currentTime ?? 0);
 //   player.setQuality(playerState.quality ?? 'auto');
 //   if (playerState.isPlay) {
-//     // Mute because of browser autoplay policy
 //     player.setVolume(0);
 //     player.play();
 //   }
@@ -23,37 +44,40 @@
 // setPlayerStateFromLocalStorage();
 
 // const updateLocalStorage = () =>
-//   localStorage.setItem(PLAYER_STATE_LS_KEY, JSON.stringify(playerState));
+//   localStorage.setItem(PLAYER_STATE, JSON.stringify(playerState));
 
-// const onPlayerTimeUpdate = ({ seconds }) => {
+// const timeUpdate = ({ seconds }) => {
 //   playerState.currentTime = seconds;
 //   updateLocalStorage();
 // };
 
-// const onPlayerPlay = () => {
+// const playerPlay = () => {
 //   playerState.isPlay = true;
 //   updateLocalStorage();
 // };
 
-// const onPlayerPause = ({ seconds }) => {
+// const playerPause = ({ seconds }) => {
 //   playerState.currentTime = seconds;
 //   playerState.isPlay = false;
 //   updateLocalStorage();
 // };
 
-// const onPlayerQualityChange = ({ quality }) => {
+// const playerQuality = ({ quality }) => {
 //   playerState.quality = quality;
 //   updateLocalStorage();
 // };
 
-// const onPlayerVideoEnded = () => {
-//   // Timeout because of throtle
-//   setTimeout(() => localStorage.removeItem(PLAYER_STATE_LS_KEY), 1000);
+// const videoEnded = () => {
+//   setTimeout(() => localStorage.removeItem(PLAYER_STATE), 1000);
 //   playerState = {};
 // };
 
-// player.on('timeupdate', throtle(onPlayerTimeUpdate, 1000));
-// player.on('play', onPlayerPlay);
-// player.on('pause', onPlayerPause);
-// player.on('qualitychange', onPlayerQualityChange);
-// player.on('ended', onPlayerVideoEnded);
+// player.on('timeupdate', throtle(timeUpdate, 1000));
+// player.on('play', playerPlay);
+// player.on('pause', playerPause);
+// player.on('qualitychange', playerQuality);
+// player.on('ended', videoEnded);
+
+
+
+
